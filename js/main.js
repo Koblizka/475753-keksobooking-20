3a346ba4afdd3054d50f3e932ae4397999f8acdd
Koblizka <flood.nya@gmail.com>
2020-06-10 23:41:33 +0300

    improves task

--- js/main.js
+++ js/main.js
@@ -49,15 +49,47 @@ var OFFER = {
     'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
   ],
   map: {
-    min: 130,
-    max: 630
+    minX: 20,
+    maxX: 740,
+    minY: 130,
+    maxY: 630
   }
 };
+var OFFER_TITLES = [
+  'Вот это самый чёткий вариант',
+  'Если хочешь страдать, то ты пришёл по адресу',
+  'Можем вместе потусить — пива воспить',
+  'Чётче моей хатки, только бобровая хатка',
+  'Не забывай оглядываться когда чистишь зубы',
+  'Воздух платный, холодильник тоже',
+  'Пшек пржек, ПШЕК вржек',
+  'Мир, дверь, мяч!',
+];
+var OFFER_DESCRIPTIONS = [
+  'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована',
+  'Если хочешь страдать, то ты пришёл по адресу. Ну как, хотите стать королем или королевой? Звоните!!!',
+  'Можем Мы приглашаем вас окунуться в сказочный мир в прямом смысле этого слова!»',
+  'Не „Баунти“, но тоже „райское наслаждение“…',
+  'Здание было построено в 1895 году известным архитектором для своей семьи, что объясняет качество строения. Дом расположен у холма Петржин, на берегу Влтавы, в районе Мала Страна. Петржин, без сомнения, лучшее место для тех, кто хотел бы находиться в самом центре города и наслаждаться прогулками по красивому парку.',
+  'За домом находится средневековая оборонительная стена Праги «Стена голода». О истории возникновения стены известно то, что Карл IV в 1361 году, во времена засухи и неурожая, повысил цены на хлеб. ',
+  'Позже, на месте танка построили фонтан, однако его маленький кусочек до сих пор находится на площади и напоминает горожанам о действиях прошлых лет в Праге',
+  'Our holiday was wonderful. Everything needed was in the apartment. A lot of dishes, tea, coffee, sugar, salt, coffee maker. Perfect cleaning. Windows overlook the courtyard, quiet at night. Thanks you.!',
+];
 
-// Возвращает путь до аватара пользователя
-var getAvatarAddress = function (number) {
-  return 'img/avatars/user' + (number < 10 ? '0' + number : number) + '.png';
+
+// Возвращает массив аватарок пользователя
+var getAvatarAddresses = function () {
+  var addresses = [];
+
+  for (var i = 1; i <= OFFERS_COUNT; i += 1) {
+    addresses.push('img/avatars/user' + i.toString().padStart(2, '0') + '.png');
+  }
+
+  return addresses;
 };
+
+// Получаем массив адрессов аватарок
+var avatarAddress = getAvatarAddresses();
 
 // Возвращает псевдорандомное чилсо в заданном диапозоне
 var getRandomNumber = function (min, max) {
@@ -65,12 +97,12 @@ var getRandomNumber = function (min, max) {
 };
 
 // Возвращает рандомное значение
-var getRandomElement = function (object) {
-  return object[getRandomNumber(0, object.length)];
+var getRandomElement = function (array) {
+  return array[getRandomNumber(0, array.length)];
 };
 
 // Перемещивание Фишера-Йетса
-var shuffleFisherYets = function (array) {
+var shuffle = function (array) {
   var j = 0;
   var temp = 0;
 
@@ -85,26 +117,26 @@ var shuffleFisherYets = function (array) {
 };
 
 // Подготавливает предложение
-var makeOffer = function (offerData, index) {
-  var locationX = getRandomNumber(0, document.querySelector('.map').offsetWidth);
-  var locationY = getRandomNumber(OFFER.map.min, OFFER.map.max);
+var makeOffer = function () {
+  var locationX = getRandomNumber(OFFER.map.minX, OFFER.map.maxX);
+  var locationY = getRandomNumber(OFFER.map.minY, OFFER.map.maxY);
 
   return {
     authors: {
-      avatar: getAvatarAddress(index)
+      avatar: avatarAddress.shift()
     },
     offer: {
-      title: 'Offer title - ' + index,
+      title: shuffle(OFFER_TITLES).shift(),
       address: locationX + ', ' + locationY,
-      price: getRandomNumber(offerData.prices.min, offerData.prices.max),
-      type: getRandomElement(offerData.types),
-      rooms: getRandomElement(offerData.rooms),
-      guests: getRandomElement(offerData.guests),
-      checkin: getRandomElement(offerData.times),
-      checkout: getRandomElement(offerData.times),
-      features: getRandomElement(offerData.features),
-      description: 'Randome Lorem - ' + index,
-      photos: getRandomElement(offerData.photos)
+      price: getRandomNumber(OFFER.prices.min, OFFER.prices.max),
+      type: getRandomElement(OFFER.types),
+      rooms: getRandomElement(OFFER.rooms),
+      guests: getRandomElement(OFFER.guests),
+      checkin: getRandomElement(OFFER.times),
+      checkout: getRandomElement(OFFER.times),
+      features: shuffle(OFFER.features).slice(0, getRandomNumber(1, OFFER.features.length)),
+      description: shuffle(OFFER_DESCRIPTIONS),
+      photos: shuffle(OFFER.photos).slice(0, getRandomNumber(1, OFFER.photos.length)),
     },
     location: {
       x: locationX,
@@ -114,28 +146,35 @@ var makeOffer = function (offerData, index) {
 };
 
 //  Генерирует список предложений
-var generateOffers = function (number, data) {
+var generateOffers = function () {
   var offers = [];
 
-  for (var i = 1; i <= number; i += 1) {
-    offers.push(makeOffer(data, i));
+  for (var i = 1; i <= OFFERS_COUNT; i += 1) {
+    offers.push(makeOffer());
   }
 
   return offers;
 };
 
 // Получаем список предложений
-var offers = generateOffers(OFFERS_COUNT, OFFER);
+var offers = generateOffers();
 
 // Перемещиваем массив предложений
-shuffleFisherYets(offers);
+shuffle(offers);
+
+// Получаем ссылку на карту
+var map = document.querySelector('.map');
+// Получаем ссылку на pin
+var pin = document.querySelector('#pin');
+// Получаем ссылку на блок пинов на карте
+var mapPins = document.querySelector('.map__pins');
 
 // Убираем затемнение с карты
-document.querySelector('.map').classList.remove('map--faded');
+map.classList.remove('map--faded');
 
 // Подготавливаем шаблон пина
-var preparePin = function (pinData) {
-  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var makePin = function (pinData) {
+  var pinTemplate = pin.content.querySelector('.map__pin');
   var pinImg = pinTemplate.querySelector('img');
 
   pinTemplate.setAttribute('style', 'left: ' + (pinData.location.x - PIN_OFFSET.x) + 'px; ' +
@@ -147,15 +186,15 @@ var preparePin = function (pinData) {
 };
 
 // Рендер пинов на странице
-var renderPins = function (pinSetup) {
+var renderPins = function (pinSetups) {
   var fragmentPin = document.createDocumentFragment();
 
-  for (var i = 0; i < pinSetup.length; i += 1) {
-    var pin = preparePin(pinSetup[i]).cloneNode(true);
-    fragmentPin.appendChild(pin);
+  for (var i = 0; i < pinSetups.length; i += 1) {
+    var tempPin = makePin(pinSetups[i]).cloneNode(true);
+    fragmentPin.appendChild(tempPin);
   }
 
-  document.querySelector('.map__pins').appendChild(fragmentPin);
+  mapPins.appendChild(fragmentPin);
 };
 
 // Вызываем рендер пинов
